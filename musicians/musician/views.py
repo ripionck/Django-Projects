53001fe8eb77c518320c299ef84c1b9e566ad608
Ripion Chakma <ripionchakma.421@gmail.com>
2023-12-08 15:58:54 +0600

    musician and album created

---
+++ musicians/musician/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from . import forms
+from . import models
+
+# Create your views here.
+
+
+def add_musician(request):
+    if request.method == 'POST':  # if user click submit button
+        musician_form = forms.MusicianForm(request.POST)
+        if musician_form.is_valid():
+            musician_form.save()
+            return redirect('add_musician')
+
+    else:  # user normally website e gele blank form pabe
+        musician_form = forms.MusicianForm()
+    return render(request, 'add_musician.html', {'form': musician_form})
+
+
+def edit_musician(request, id):
+    musician = models.Musician.objects.get(pk=id)
+    musician_form = forms.MusicianForm(instance=musician)
+    # print(post.title)
+    if request.method == 'POST':  # if user click submit button
+        musician_form = forms.MusicianForm(request.POST)
+        if musician_form.is_valid():
+            musician_form.save()
+            return redirect('homepage')
+
+    return render(request, 'add_musician.html', {'form': musician_form})
